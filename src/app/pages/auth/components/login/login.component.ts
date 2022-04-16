98443ea8447fc8564672410a104b5820a79ca192
Anirban barman <anirbandali247@gmail.com>
2022-04-16 21:35:22 +0530

    validation added

--- src/app/pages/auth/components/login/login.component.ts
+++ src/app/pages/auth/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from 'src/app/services/auth.service';
 
@@ -34,7 +35,8 @@ export class LoginComponent implements OnInit {
 
   }
 
-  logIn() {
+  logIn(form:NgForm) {
+    console.log(form)
 
     this.authService.adminLogin(this.loginForm).subscribe((response) => {
       console.log("login successful")
